b1209232ec27321ce91a8c93a2b67695d5c9ac83
markst0 <133260129+markst0@users.noreply.github.com>
2024-11-18 22:01:40 +0300

    Update test.java

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class test {
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpressionEvaluatorTest {
+
+    private Map<String, Double> testVariables;
+
+    @BeforeEach
+    void setUp() {
+        testVariables = new HashMap<>();
+        testVariables.put("x", 10.0);
+        testVariables.put("y", 5.0);
+    }
+
+    @Test
+    void testEvaluateSimpleExpression() throws Exception {
+        assertEquals(15.0, ExpressionEvaluator.evaluate("10 + 5"));
+    }
+
+    @Test
+    void testEvaluateWithVariables() throws Exception {
+        ExpressionEvaluator.variables.putAll(testVariables);
+        assertEquals(15.0, ExpressionEvaluator.evaluate("x + y"));
+    }
+
+    @Test
+    void testEvaluateWithParentheses() throws Exception {
+        assertEquals(30.0, ExpressionEvaluator.evaluate("(10 + 5) * 2"));
+    }
+
+    @Test
+    void testEvaluateWithDivision() throws Exception {
+        assertEquals(2.0, ExpressionEvaluator.evaluate("10 / 5"));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(Exception.class, () -> {
+            ExpressionEvaluator.evaluate("10 / 0");
+        });
+        assertEquals("Деление на ноль!", exception.getMessage());
+    }
+
+    @Test
+    void testUndefinedVariable() {
+        Exception exception = assertThrows(Exception.class, () -> {
+            ExpressionEvaluator.evaluate("z + 5");
+        });
+        assertEquals("Переменная не определена: z", exception.getMessage());
+    }
 }
